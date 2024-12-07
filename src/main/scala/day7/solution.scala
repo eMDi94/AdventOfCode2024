7a0961b8d8d629283490c6da7c5e0e14cc96fdbb
Marco Dalai <mdalai2394@gmail.com>
2024-12-07 22:27:38 +0100

    Day7

--- src/main/scala/day7/solution.scala
+++ src/main/scala/day7/solution.scala
@@ -2,7 +2,6 @@ package day7
 
 import scala.io.Source
 
-
 @main def main(): Unit =
   val input = Source.fromResource("day7/input.txt").getLines
   val parsedInput = input.map(parseLine)
@@ -14,3 +13,10 @@ import scala.io.Source
     .foldLeft(0L)((acc, equation) => acc + equation.result)
 
   println(s"The result is $result")
+
+def parseLine(line: String): Option[Equation] =
+  line.split(":") match
+    case Array(result, numbers, _*) =>
+      val parsedNumbers = numbers.split(" ").filter(_.nonEmpty).map(_.trim.toLong)
+      Some(Equation(result.trim.toLong, parsedNumbers.toList))
+    case _ => None
